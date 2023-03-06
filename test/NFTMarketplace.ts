b0698ff7f383ae4cbef9b578ae31ead7f1e0f628
Baltasar Romero <44850763+baltasarromero@users.noreply.github.com>
2023-03-06 09:58:20 -0300

    Modifiers (#2)
    * Skeleton Contract
    
    * Added modifiers - Changed mapping keys to bytes32

--- test/NFTMarketplace.ts
+++ test/NFTMarketplace.ts
@@ -1,35 +1,31 @@
-import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers"
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs"
+import { expect } from "chai"
+import { ethers } from "hardhat"
 
 describe("NFTMarketplace", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployCleanNFMarketplaceFixture() {
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    // We define a fixture to reuse the same setup in every test.
+    // We use loadFixture to run this setup once, snapshot that state,
+    // and reset Hardhat Network to that snapshot in every test.
+    async function deployCleanNFMarketplaceFixture() {
+        // Contracts are deployed using the first signer/account by default
+        const [owner, otherAccount] = await ethers.getSigners()
 
-    const NFTMarketplace = await ethers.getContractFactory("NFTMarketplace");
-    const nftMarketplace = await NFTMarketplace.deploy();
+        const NFTMarketplace = await ethers.getContractFactory("NFTMarketplace")
+        const nftMarketplace = await NFTMarketplace.deploy()
 
-    return {owner, otherAccount };
-  }
+        return { owner, otherAccount }
+    }
 
-  async function deployNFMarketplaceWithDataFixture() {
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    async function deployNFMarketplaceWithDataFixture() {
+        // Contracts are deployed using the first signer/account by default
+        const [owner, otherAccount] = await ethers.getSigners()
 
-    const NFTMarketplace = await ethers.getContractFactory("NFTMarketplace");
-    const nftMarketplace = await NFTMarketplace.deploy();
+        const NFTMarketplace = await ethers.getContractFactory("NFTMarketplace")
+        const nftMarketplace = await NFTMarketplace.deploy()
 
-    return {owner, otherAccount };
-  }
+        return { owner, otherAccount }
+    }
 
-  describe("Deployment", function () {
-   
-  });
-
- 
-});
+    describe("Deployment", function () {})
+})
